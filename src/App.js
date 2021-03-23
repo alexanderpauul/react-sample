b399a1360baa59c775840e1aaf76f55f6925704a
Alexander Paulino <alexanderpauul@hotmail.com>
2021-03-22 22:43:00 -0400

    Validation and error message
    add validation form and error messages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { isEmpty } from "lodash";
+import { isEmpty, size } from "lodash";
 import React, { useState } from "react";
 import shortid from "shortid";
 
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null);
+
+  const ValidateForm = () => {
+    let isValid = true;
+    setError(null);
+    if (isEmpty(task)) {
+      setError("Debes ingresar una tarea.");
+      isValid = false;
+    }
+
+    return isValid;
+  };
 
   const addTask = (e) => {
     e.preventDefault();
-    if (isEmpty(task)) {
-      console.log("Task empty");
+
+    if (!ValidateForm()) {
       return;
     }
 
@@ -22,6 +36,34 @@ function App() {
     setTask("");
   };
 
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTask);
+  };
+
+  const editTask = (parmTask) => {
+    setTask(parmTask.name);
+    setEditMode(true);
+    setId(parmTask.id);
+  };
+
+  const saveTask = (e) => {
+    e.preventDefault();
+
+    if (!ValidateForm()) {
+      return;
+    }
+
+    const editedTasks = tasks.map((item) =>
+      item.id === id ? { id, name: task } : item
+    );
+
+    setTasks(editedTasks);
+    setEditMode(false);
+    setTask("");
+    setId("");
+  };
+
   return (
     <div className="container mt-5">
       <h1>Tasks</h1>
@@ -31,25 +73,36 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Tasks List</h4>
 
-          <ul className="list-group">
-            {tasks.map((task) => (
-              <li className="list-group-item" key={task.id}>
-                <span className="lead">{task.name}</span>
-                <button className="btn btn-danger btn-sm float-right mx-2">
-                  Delete
-                </button>
-                <button className="btn btn-warning btn-sm float-right">
-                  Edit
-                </button>
-              </li>
-            ))}
-          </ul>
+          {size(tasks) == 0 ? (
+            <h5 className="text-center">There no task...</h5>
+          ) : (
+            <ul className="list-group">
+              {tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm float-right"
+                    onClick={() => editTask(task)}
+                  >
+                    Edit
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
 
         <div className="col-4">
-          <h4 className="text-center">Form</h4>
+          <h4 className="text-center">{editMode ? "Edit task" : "Add Task"}</h4>
 
-          <form onSubmit={addTask}>
+          <form onSubmit={editMode ? saveTask : addTask}>
+            {error && <span className="text-danger">{error}</span>}
             <input
               type="text"
               className="form-control mb-2"
@@ -58,8 +111,15 @@ function App() {
               value={task}
             />
 
-            <button type="submit" className="btn btn-dark btn-block">
-              Add
+            <button
+              type="submit"
+              className={
+                editMode
+                  ? "btn btn-warning btn-block"
+                  : "btn btn-dark btn-block"
+              }
+            >
+              {editMode ? "Edit" : "Add"}
             </button>
           </form>
         </div>
